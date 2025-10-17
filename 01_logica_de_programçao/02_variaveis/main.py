d8ebbab1797ecbeaa4314b53cf21b9a603cbaa8d
cauecaetano <caue.caetano.santos@gmail.com>
2025-10-17 16:40:11 -0300

    Meu primeiro commit . Aula do dia 17/10/2025.

---
+++ 01_logica_de_programçao/02_variaveis/main.py
@@ -0,0 +1,10 @@
+# declaração de variáveis 
+nome_completo = "Cauê" #string 
+idade = 17 # int
+altura = 1.67 #float
+programador = True # boll (boolean)
+# saída de dados 
+print(nome_completo)
+print(idade)
+print(altura)
+print(programador)
---
+++ 01_logica_de_programçao/03_tipos_de_dados/main.py
@@ -0,0 +1,11 @@
+# declaração de variáveis
+nome = "Cauê" 
+idade = 17
+altura = 1.66
+programador = True 
+
+# exibe os tipos de dados 
+print(type(nome))
+print(type(idade))
+print(type(altura))
+print(type(programador))
