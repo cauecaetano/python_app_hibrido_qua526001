44f7f9799f7ab880a767bf43c2070fc678325c93
cauecaetano <caue.caetano.santos@gmail.com>
2025-12-09 17:32:53 -0300

    aula dia 09

--- 07_app_hibrido/06_to_do_app/src/main.py
+++ 07_app_hibrido/06_to_do_app/src/main.py
@@ -1,36 +1,160 @@
 import flet as ft
 
-def main(page: ft.Page):
-    def add_clicked(e):
-        tasks_view.controls.append(ft.Checkbox(label=new_task.value))
-        new_task.value = ""
-        view.update()
+class Task(ft.Column):
+    def __init__(self, task_name, task_status_change, task_delete):
+        super().__init__()
+        self.completed = False
+        self.task_name = task_name
+        self.task_status_change = task_status_change
+        self.task_delete = task_delete
+        
+        self.display_task = ft.Checkbox(value=False, label=self.task_name, on_change=self.status_changed)
+        self.edit_name = ft.TextField(expand=1)
 
-    new_task = ft.TextField(hint_text="O que precisa ser feito?", expand=True)
-    tasks_view = ft.Column()
-    view=ft.Column(
-        width=600,
-        controls=[
+        self.display_view = ft.Row(
+            alignment=ft.MainAxisAlignment.SPACE_BETWEEN,
+            vertical_alignment=ft.CrossAxisAlignment.CENTER,
+            controls=[
+                self.display_task,
+                ft.Row(
+                    spacing=0,
+                    controls=[
+                        ft.IconButton(
+                            icon=ft.Icons.CREATE_OUTLINED,
+                            tooltip="Edit To-Do",
+                            on_click=self.edit_clicked,
+                        ),
+                        ft.IconButton(
+                            ft.Icons.DELETE_OUTLINE,
+                            tooltip="Delete To-Do",
+                            on_click=self.delete_clicked,
+                        ),
+                    ],
+                ),
+            ],
+        )
+
+        self.edit_view = ft.Row(
+            visible=False,
+            alignment=ft.MainAxisAlignment.SPACE_BETWEEN,
+            vertical_alignment=ft.CrossAxisAlignment.CENTER,
+            controls=[
+                self.edit_name,
+                ft.IconButton(
+                    icon=ft.Icons.DONE_OUTLINE_OUTLINED,
+                    icon_color=ft.Colors.GREEN,
+                    tooltip="Update To-Do",
+                    on_click=self.save_clicked,
+                ),
+            ],
+        )
+        
+        self.controls = [self.display_view, self.edit_view]
+
+    def edit_clicked(self, e):
+        self.edit_name.value = self.display_task.label
+        self.display_view.visible = False
+        self.edit_view.visible = True
+        self.update()
+
+    def save_clicked(self, e):
+        self.display_task.label = self.edit_name.value
+        self.display_view.visible = True
+        self.edit_view.visible = False
+        self.update()
+    
+    def status_changed(self, e):
+        self.completed = self.display_task.value
+        self.task_status_change()
+
+    def delete_clicked(self, e):
+        self.task_delete(self)
+
+class TodoApp(ft.Column):
+    def __init__(self):
+        super().__init__()
+        self.new_task = ft.TextField(hint_text="O que fazer agora?", expand=True)
+        self.tasks = ft.Column()
+
+        self.filter = ft.Tabs(
+            selected_index=0,
+            on_change=self.tabs_changed,
+            tabs=[ft.Tab(text="all"), ft.Tab(text="activate"), ft.Tab(text="completed")],
+        )
+
+        self.items_left = ft.Text("0 items left")
+
+        self.width = 600
+        self.controls = [
+            ft.Row(
+                [ft.Text(value="Todos", theme_style=ft.TextThemeStyle.HEADLINE_MEDIUM)],
+                alignment=ft.MainAxisAligment.Center,
+            ),
             ft.Row(
                 controls=[
-                    new_task,
-                    ft.FloatingActionButton(icon=ft.Icons.ADD, on_click=add_clicked),
+                    self.new_task,
+                    ft.FloatingActionButton(
+                        icon=ft.Icons.ADD,
+                        on_click=self.add_clicked
+                    ),
                 ],
             ),
-            tasks_view,
-        ],
-    )
-
-    page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
-    page.add(view)
-
-    page.add(
-        ft.SafeArea(
-            ft.Container(
-                alignment=ft.alignment.center,
+            ft.Column(
+                spacing=25,
+                control=[
+                    self.filter,
+                    self.tasks,
+                    ft.Row(
+                        alignment=ft.MainAxisAlignment.SPACE_BETWEEN,
+                        vertical_alignment=ft.CrossAxisAlignment.CENTER,
+                        controls=[
+                            self.items_left,
+                            ft.OutlinedButton(
+                                text="Clear completed",
+                                on_click=self.clear_clicked
+                            ),
+                        ],
+                    ),
+                ],
             ),
-            expand=True,
-        )
-    )
+        ]
+    
+    def add_clicked(self, e):
+        task = Task(self.new_task.value, self.task_status_change, self.task_delete)
+        self.tasks.controls.append(task)
+        self.new_task.value = ""
+        self.update()
+    
+    def task_delete(self, task):
+        self.tasks.controls.remove(task)
+        self.update()
+
+    def tabs_changed(self, e):
+        self.update()
+
+    def task_status_change(self, e):
+        self.update()
+
+    def add_clicked(self, e):
+        task = Task(self.new_task.value, self.task_status_change, self.task_delete)
+
+    def before_update(self):
+        status = self.filter.tabs[self.filter.selected_index].text
+        for task in self.tasks.controls:
+            task.visible = (
+                status == "all"
+                or (status == "active" and task.completed == False)
+                or (status == "completed" and task.completed)
+            )
+
+def main(page: ft.Page):
+    page.title = "To-Do App"
+    page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
+    page.update()
+
+    todo = TodoApp()
+
+    page.add(todo)
+
 
 ft.app(main)
